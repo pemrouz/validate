9262e1fb31949a9ac812633b94409171efe45ae7
pemrouz <p3pedram@hotmail.com>
2016-07-24 13:15:27 +0100

    0.0.1

--- dist/client.js
+++ dist/client.js
@@ -17,10 +17,7 @@ function validate(_ref) {
   o.on('blur.interacted', function (d) {
     return o.draw(validation.interacted = true);
   }).classed('is-interacted', interacted).classed('is-invalid', !(validation.valid = pattern.test ? pattern.test(value) : pattern.call(this, value)));
-  console.log("validation.valid", validation.valid);
-  console.log("pattern.test", pattern.test);
-  console.log("value", value);
-  console.log("pattern.test(value) ", pattern.test(value));
+
   o('.invalid-message', message).text(str);
 }
 },{}],2:[function(require,module,exports){
