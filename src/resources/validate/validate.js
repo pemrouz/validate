4da90122c732cf7ec580a7b694352a44c73d2ff2
pemrouz <p3pedram@hotmail.com>
2016-09-03 16:59:46 +0100

    fix: change component signature (websdk/vanilla#2)

--- src/resources/validate/validate.js
+++ src/resources/validate/validate.js
@@ -1,12 +1,15 @@
-export default function validate({ value = '', validation }) { 
+export default function validate(node, { value = '', validation }) { 
   const { pattern, message, interacted } = validation
-      , o = once(this)
+      , o = once(node)
 
-  o.on('blur.interacted', d => o.draw(validation.interacted = true))
+  o.on('blur.interacted', (d, i, el, e) => {
+    if (d && d.focused) return
+    o.draw(validation.interacted = true)
+  })
     .classed('is-interacted', interacted)
     .classed('is-invalid', !(validation.valid = pattern.test 
       ? pattern.test(value) 
-      : pattern.call(this, value)))
+      : pattern.call(node, value)))
 
   o('.invalid-message', message)
     .text(str)
